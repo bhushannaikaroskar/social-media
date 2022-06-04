028c94d6b900ee3be8b5321aa1db780701179383
bhushannaikaroskar <bhushannaikaroskar@gmail.com>
2022-06-04 16:31:36 +0530

    add: tailwind css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {makeServer} from "./server"
+
+makeServer();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
